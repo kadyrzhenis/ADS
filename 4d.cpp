2c8d7f9de408ae21518cb56ad1ca2bd880264187
kadyrzhenis <kadyrkulovzhenis000509@gmail.com>
2021-02-15 17:41:31 +0600

    test time

---
+++ 4d.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,m,cnt=0;
+    cin>>n>>m;
+    int arr[m][5];
+    for(int i=0;i<m;++i){
+        for(int j=0;j<2;++j){
+            cin>>arr[i][j];
+        }
+    }  for(int i=0;i<m;++i){
+        for(int j=0;j<2;++j){
+           if(arr[i][0]==i+1 )
+        }
+    }
+
+
+}
